c1ac21fcfb9f8a59892e55ff7e755f5c7ccb9125
kruthikha <01fe22bca164@kletech.ac.in>
2024-01-17 16:59:43 +0530

    Code for Basic Arithmatic Ops

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+/**
+ * The Calculator class is used for performing mathematical calculations.
+ */
+class Calculator{
+    float addition(float operand_1,float operand_2){
+        return(operand_1 + operand_2);
+    }
+    float subtraction(float operand_1,float operand_2){
+        return(operand_1 - operand_2);
+    }
+float multiplication(float operand_1,float operand_2){
+    return(operand_1 * operand_2);
+}
+
+float division(float operand_1,float operand_2){
+    return(operand_1 / operand_2);
+}
+
+ public static void main(String[] args) {
+    Calculator calc=new Calculator();
+    float operand_1=10;
+    float operand_2=5;
+    float add_result=calc.addition(operand_1,operand_2);
+    System.out.println("result of addition"+add_result);
+    float sub_result=calc.subtraction(operand_1,operand_2);
+    System.out.println("result of addition"+sub_result);
+    float mult_result=calc.multiplication(operand_1,operand_2);
+    System.out.println("result of addition"+mult_result);
+    float div_result=calc.division(operand_1,operand_2);
+    System.out.println("result of addition"+div_result);
+}
+}
