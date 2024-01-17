f5c8fa974dff2ab9a444ddfa9d4b75e76abc539e
kruthikha <01fe22bca164@kletech.ac.in>
2024-01-17 17:38:41 +0530

    Updated code for Square functionality

--- Calculator.java
+++ Calculator.java
@@ -15,7 +15,9 @@ float multiplication(float operand_1,float operand_2){
 float division(float operand_1,float operand_2){
     return(operand_1 / operand_2);
 }
-
+float square(float operand_1){
+    return(operand_1*operand_1);
+}
  public static void main(String[] args) {
     Calculator calc=new Calculator();
     float operand_1=10;
@@ -28,5 +30,9 @@ float division(float operand_1,float operand_2){
     System.out.println("result of addition"+mult_result);
     float div_result=calc.division(operand_1,operand_2);
     System.out.println("result of addition"+div_result);
+    float squ_result=calc.square(operand_1);
+    System.out.println("result of addition"+squ_result);
+    
+
 }
 }
