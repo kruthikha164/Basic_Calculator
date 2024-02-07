ff9c440a989f440c87e00c2d8797168d82dfdfba
kruthikha <01fe22bca164@kletech.ac.in>
2024-02-07 15:34:26 +0530

    program updated

---
+++ Extendedclass.java
@@ -0,0 +1,37 @@
+/**
+ * The Extendedclass is a Java class that needs to be further defined.
+ */
+public class Extendedclass {
+    // The code defines three methods in the `Extendedclass` class:
+   /**
+    * The function "square" takes a float operand and returns the square of that operand.
+    * 
+    * @param operand_1 A float value that represents the operand to be squared.
+    * @return the square of the operand_1.
+    */
+    float square(float operand_1){
+        return(operand_1*operand_1);
+    }
+    /**
+     * The function "cube" takes a float number as input and returns the cube of that number.
+     * 
+     * @param operand_1 The parameter "operand_1" is a float value that represents the number that we
+     * want to calculate the cube of.
+     * @return The cube of the operand_1 value.
+     */
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+   /**
+    * The function calculates the modulus of two floating-point numbers.
+    * 
+    * @param operand_1 The first operand of the modulo operation.
+    * @param operand_2 The operand_2 parameter is the divisor in the modulo operation. It is the number
+    * by which the operand_1 is divided to find the remainder.
+    * @return the remainder of dividing operand_1 by operand_2.
+    */
+    float mod(float operand_1, float operand_2){
+        return(operand_1 % operand_2);
+    }
+    
+}
